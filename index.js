c5802b25e772371254893bc499c116198b87746f
Bryan Phillips <bphillips@gopro.com>
2016-12-12 11:57:20 -0800

    logs

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 console.log('Loading function');
 
 exports.handler = function(event, context) {
+  console.log('EVENT', event);
+  console.log('CONTEXT', context);
     //Echo back the text the user typed in
     context.succeed('You sent: ' + event.text);
 };
