2e500dd4d803887bc8e197eb531dd8a66d71ba6c
Bryan Phillips <bphillips@gopro.com>
2016-12-12 18:09:55 -0800

    adding video search

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 var Botkit = require('botkit');
+var videoSearch = require('./video-search');
 
 if (!process.env.CLIENT_ID || !process.env.CLIENT_SECRET || !process.env.PORT || !process.env.VERIFICATION_TOKEN) {
     console.log('Error: Specify CLIENT_ID, CLIENT_SECRET, VERIFICATION_TOKEN and PORT in environment');
@@ -38,21 +39,12 @@ controller.setupWebserver(process.env.PORT, function (err, webserver) {
 });
 
 
-//
-// BEGIN EDITING HERE!
-//
-
 controller.on('slash_command', function (slashCommand, message) {
 
     switch (message.command) {
-        case "/gopro": //handle the `/echo` slash command. We might have others assigned to this app too!
-            // The rules are simple: If there is no text following the command, treat it as though they had requested "help"
-            // Otherwise just echo back to them what they sent us.
-
-            // but first, let's make sure the token matches!
+        case "/gopro":
             if (message.token !== process.env.VERIFICATION_TOKEN) return; //just ignore it.
 
-            // if no text was supplied, treat it as a help command
             if (message.text === "" || message.text === "help") {
                 slashCommand.replyPrivate(message,
                     "I will send you videos. " +
@@ -60,14 +52,13 @@ controller.on('slash_command', function (slashCommand, message) {
                 return;
             }
 
-            // If we made it here, just echo what the user typed back at them
-            //TODO You do it!
-            slashCommand.replyPublic('Surf\'s up!', "https://www.youtube.com/watch?v=HBklS2vYEPo");
-
+            videoSearch()
+              .then(function(url){
+                slashCommand.replyPublic('Message here!!', url);
+              });
             break;
         default:
             slashCommand.replyPublic(message, "I'm afraid I don't know how to " + message.command + " yet.");
-
     }
 
 })
