39655f239c496dd7413fc876c0839de67b287524
Bryan Phillips <bphillips@gopro.com>
2016-12-12 23:45:14 -0800

    trying to configure bot

--- src/index.js
+++ src/index.js
@@ -1,6 +1,30 @@
 var Botkit = require('botkit');
 var videoSearch = require('./video-search');
 
+function onInstallation(bot, installer) {
+    if (installer) {
+        bot.startPrivateConversation({user: installer}, function (err, convo) {
+            if (err) {
+                console.log(err);
+            } else {
+                convo.say('I am a bot that has just joined your team');
+                convo.say('You must now /invite me to a channel so that I can be of use!');
+            }
+        });
+    }
+}
+
+var _bots = {};
+
+function _trackBot(bot) {
+    _bots[bot.config.token] = bot;
+}
+
+function die(err) {
+    console.log(err);
+    process.exit(1);
+}
+
 if (!process.env.CLIENT_ID || !process.env.CLIENT_SECRET || !process.env.PORT || !process.env.VERIFICATION_TOKEN) {
     console.log('Error: Specify CLIENT_ID, CLIENT_SECRET, VERIFICATION_TOKEN and PORT in environment');
     process.exit(1);
@@ -23,6 +47,7 @@ var controller = Botkit.slackbot(config).configureSlackApp(
         clientId: process.env.CLIENT_ID,
         clientSecret: process.env.CLIENT_SECRET,
         scopes: ['commands'],
+        verification: process.env.VERIFICATION_TOKEN
     }
 );
 
@@ -40,7 +65,6 @@ controller.setupWebserver(process.env.PORT, function (err, webserver) {
 
 
 controller.on('slash_command', function (slashCommand, message) {
-
     switch (message.command) {
         case "/gopro":
             if (message.token !== process.env.VERIFICATION_TOKEN) return; //just ignore it.
@@ -63,7 +87,7 @@ controller.on('slash_command', function (slashCommand, message) {
             }
 
             if (message.text === "account-center") {
-                slashCommand.replyPrivate(message, 'Modify your account settings in GoPro Account Center: ' + 'https://gopro.com/account');
+                slashCommand.replyPrivate(message, 'Modify your account settings and manage your Plus Subscription in GoPro Account Center: ' + 'https://gopro.com/account');
                 return;
             }
 
@@ -75,6 +99,26 @@ controller.on('slash_command', function (slashCommand, message) {
         default:
             slashCommand.replyPrivate(message, "I'm afraid I don't know how to " + message.command + " yet.");
     }
+});
 
-})
-;
+controller.on('create_bot', function (bot, config) {
+    console.log('bot being created');
+    if (_bots[bot.config.token]) {
+        // already online! do nothing.
+    } else {
+
+        bot.startRTM(function (err) {
+            if (err) {
+                die(err);
+            }
+
+            _trackBot(bot);
+
+            onInstallation(bot, config.createdBy);
+        });
+    }
+});
+
+controller.hears('hello', 'direct_message', function (bot, message) {
+    bot.reply(message, 'Hello!');
+});
